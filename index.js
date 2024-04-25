7e7e3e5a9863800ad948d37e99a06b973f1cee0f
Manas Khanna <manaskhanna611@gmail.com>
2024-04-25 18:52:59 +0530

    commit

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const cors = require('cors')
 const DetailsModel = require('./models/CarDetails')
 const dontenv = require('dotenv').config()
 const port = process.env.PORT || 4000;
+const bodyParser = require("body-parser");
+const app = express()
 
 
 // console.log(dontenv);
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json());
 
-const app = express()
 // console.log('11');
-app.use(express.json({limit: '50mb'}))
-app.use(express.urlencoded({limit: '50mb', extended: true}))
+// app.use(express.json())
 app.use(cors())
 
 
