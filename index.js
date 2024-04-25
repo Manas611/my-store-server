5de6cf42f230bcdc3566d0dae5782a858885dca8
Manas Khanna <manaskhanna611@gmail.com>
2024-04-25 18:35:25 +0530

    commit

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 4000;
 
 const app = express()
 // console.log('11');
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({limit: '50mb', extended: true}))
 app.use(cors())
 
 
